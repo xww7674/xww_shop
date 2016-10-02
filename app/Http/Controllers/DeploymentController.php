dd80db648b118bba7c3504c7423c770c369a1ec1
JellyBool <jellybool@outlook.com>
2016-10-03 04:55:36 +0800

    add doc block

--- app/Http/Controllers/DeploymentController.php
+++ app/Http/Controllers/DeploymentController.php
@@ -5,10 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-use Log;
 
+/**
+ * Class DeploymentController
+ *
+ * @package App\Http\Controllers
+ */
 class DeploymentController extends Controller
 {
+    /**
+     * @param Request $request
+     */
     public function deploy(Request $request)
     {
         $commands = ['cd /var/www/laravel-ubuntu', 'git pull'];
@@ -23,6 +30,12 @@ class DeploymentController extends Controller
             abort(403);
         }
     }
+
+    /**
+     * @param $payload
+     * @param $signature
+     * @return bool
+     */
     private function isFromGithub($payload, $signature)
     {
         return 'sha1=' . hash_hmac('sha1', $payload, env('GITHUB_DEPLOY_TOKEN'), false) === $signature;
