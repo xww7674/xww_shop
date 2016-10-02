9c64658d2374cb60b2543b72408175110a408d79
JellyBool <jellybool@outlook.com>
2016-10-03 01:13:36 +0800

    change command and log

--- app/Http/Controllers/DeploymentController.php
+++ app/Http/Controllers/DeploymentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Log;
 
 class DeploymentController extends Controller
 {
@@ -12,9 +13,12 @@ class DeploymentController extends Controller
     {
         $commands = ['cd /var/www/laravel-ubuntu', 'sudo -Hu www-data git pull'];
         $signature = $request->header('X-Hub-Signature');
+        Log::info($signature);
         $payload = file_get_contents('php://input');
+        Log::info($payload);
         if ($this->isFromGithub($payload, $signature)) {
             foreach ($commands as $command) {
+                Log::info($command);
                 shell_exec($command);
             }
             http_response_code(200);
@@ -24,6 +28,8 @@ class DeploymentController extends Controller
     }
     private function isFromGithub($payload, $signature)
     {
-        return 'sha1=' . hash_hmac('sha1', $payload, env('GITHUB_DEPLOY_TOKEN'), false) === $signature;
+        $hash  = 'sha1=' . hash_hmac('sha1', $payload, env('GITHUB_DEPLOY_TOKEN'), false) === $signature;
+        Log::info($hash);
+        return $hash;
     }
 }
